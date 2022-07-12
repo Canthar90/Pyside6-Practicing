1975426a08142139a80737132b3096684b397cf2
canthar90 <d.cieslak90@gmail.com>
2022-07-12 09:02:01 +0200

    Practicing shape and line drawning in PySide6

--- painter.py
+++ painter.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtCore import Qt
+from random import randint, choice
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -15,9 +16,23 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
     def draw_something(self):
+        colors = ['#FFD141', '#376F9F', '#0D1F2D', '#E9EBEF', '#EB5160']
+
         canvas = self.label.pixmap()
         painter = QtGui.QPainter(canvas)
-        painter.drawLine(10, 10, 300, 200)
+        pen = QtGui.QPen()
+        pen.setWidth(3)
+        pen.setColor(QtGui.QColor('#376F9F'))
+        painter.setPen(pen)
+        painter.drawLine(
+            QtCore.QPoint(100, 100),
+            QtCore.QPoint(300, 200)
+        )
+        painter.drawRect(50, 50, 100, 100)
+        painter.drawRect(60, 60, 150, 100)
+        painter.drawRect(70, 70, 100, 150)
+        painter.drawRect(80, 80, 150, 100)
+        painter.drawRect(90, 90, 100, 150)
         painter.end()
         self.label.setPixmap(canvas)
 
