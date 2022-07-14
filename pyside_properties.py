c76f30eb7779975a6fadf7bc1141c06efd1d0876
canthar90 <d.cieslak90@gmail.com>
2022-07-14 09:07:28 +0200

    Practicing PySide6 properties

---
+++ pyside_properties.py
@@ -0,0 +1,25 @@
+from PySide6.QtCore import Property, QObject
+
+
+class CustomObject(QObject):
+    def __init__(self):
+        super().__init__()
+        self.value = 0
+
+    @Property(int)
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self,value):
+        self._value = value
+
+        if value != self._value:
+            self._value = value
+            self.valueChanged.emit(value)
+
+
+
+obj = CustomObject()
+obj.value = 7
+print(obj.value)
