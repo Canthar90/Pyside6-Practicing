678e5103f752bc3f26e6f560d0a7d3abc44e5776
canthar90 <d.cieslak90@gmail.com>
2022-07-22 13:49:52 +0200

    First attempts with miltithreading

--- combine_test.py
+++ combine_test.py
@@ -1,15 +1,25 @@
+import logging
+from multiprocessing import Process
+import time
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QGridLayout
 from checkbox import AnimatedToggle
 from barwidget import PowerBar
-from state_printer_test import CheckStates
 from PySide6 import QtCore
 
 @QtCore.Slot()
+def checking_power():
+    while True:
+        time.sleep(2)
+        print(speed_bar.value())
+    
+@QtCore.Slot()    
 def checking():
     print(main_toggle.isChecked())
     print(secondary_toggle.isChecked())
+    print(speed_bar.value())
+    print(type(speed_bar.value()))
 
-
+logging.basicConfig(level=logging.INFO)
 colors_3 = (["#5e4fa2", "#3288bd", "#66c2a5", "#abdda4", "#e6f598", "#ffffbf", "#fee08b", "#fdae61", "#f46d43", "#d53e4f", "#9e0142"])
 
 app = QApplication([])
@@ -33,8 +43,8 @@ check_button.setStyleSheet("""
     height: 50px;
     border-radius: 10px
     """)
-check_button.clicked.connect(checking)
 
+check_button.clicked.connect(checking)
 
 speed_bar = PowerBar(colors_3)
 
@@ -54,5 +64,14 @@ window.setGeometry(0, 0, 250, 450)
 main_toggle.stateChanged.connect(secondary_toggle.setChecked)
 
 
+
 window.show()
+
+if __name__ == '__main__':
+    x = Process(target=checking_power())
+    x.start()
+    x.join()
 app.exec()
+
+
+
